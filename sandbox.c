4cadf23bf2d2b45a56633e55ce808ddb06fa4c0a
unknown <taufik9c25@gmail.com>
2018-11-09 23:27:36 +0700

    lagi

--- sandbox.c
+++ sandbox.c
@@ -11,51 +11,50 @@ int main(){
     char comm[10];
     long tick = 0;
     do{
-        tick++;
         switch {
             case CompStr(comm, "GU") :{
-                
+                tick++;
                 break;
             }
             case CompStr(comm, "GD") :{
-                
+                tick++;
                 break;
             }
             case CompStr(comm, "GL") :{
-                
+                tick++;
                 break;
             }
             
             case CompStr(comm, "GR") :{
-                
+                tick++;
                 break;
             }
             case CompStr(comm, "ORDER") :{
-                
+                tick++;
                 break;
             }
             case CompStr(comm, "PUT") :{
-                
+                tick++;
                 break;
             }
             case CompStr(comm, "TAKE") :{
-                
+                tick++;
                 break;
             }
             case CompStr(comm, "CH") :{
-                
+                tick++;
                 break;
             }
             case CompStr(comm, "CT") :{
-                
+                tick++;
                 break;
             }
             case CompStr(comm, "PLACE") :{
-                
+                tick++;
                 break;
             }
             case CompStr(comm, "GIVE") :{
-                
+                tick++;
                 break;
             }
             case CompStr(comm, "RECIPE") :{
