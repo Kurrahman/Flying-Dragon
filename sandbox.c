6afbeac22eefbf0b35afbdd66c91d2c3c50cbc84
unknown <taufik9c25@gmail.com>
2018-11-09 23:27:24 +0700

    gas

--- sandbox.c
+++ sandbox.c
@@ -1,7 +1,87 @@
 #include "jam.h"
 #include "array.h"
 #include "point.h"
+#include "boolean.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+boolean CompStr(char S1[], char S2[]);
 
 int main(){
+    char comm[10];
+    long tick = 0;
+    do{
+        tick++;
+        switch {
+            case CompStr(comm, "GU") :{
+                
+                break;
+            }
+            case CompStr(comm, "GD") :{
+                
+                break;
+            }
+            case CompStr(comm, "GL") :{
+                
+                break;
+            }
+            
+            case CompStr(comm, "GR") :{
+                
+                break;
+            }
+            case CompStr(comm, "ORDER") :{
+                
+                break;
+            }
+            case CompStr(comm, "PUT") :{
+                
+                break;
+            }
+            case CompStr(comm, "TAKE") :{
+                
+                break;
+            }
+            case CompStr(comm, "CH") :{
+                
+                break;
+            }
+            case CompStr(comm, "CT") :{
+                
+                break;
+            }
+            case CompStr(comm, "PLACE") :{
+                
+                break;
+            }
+            case CompStr(comm, "GIVE") :{
+                
+                break;
+            }
+            case CompStr(comm, "RECIPE") :{
+                
+                break;
+            }
+            case CompStr(comm, "SAVE") :{
+                
+                break;
+            }
+            case CompStr(comm, "LOAD") :{
+                break;
+            }
+            case CompStr(comm, "EXIT") :{
+                break;
+            }
+        }
+    }while (!(CompStr(comm, "EXIT")));
+    return 0;
+}
 
+boolean CompStr(char S1[], char S2[]){
+    int i = 0;
+    int len = sizeof(S2);
+    while ((i < len) && (S1[i] == S2[i])){
+        i++;
+    }
+    return (i > len);
 }
--- test.c
+++ test.c
@@ -1,11 +1,14 @@
 #include "array.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(){
+    char comm[10] = "1234567890";
     ArOrder Ord;
     ArCreateEmpty(&Ord,6);
     ArAddEli(&Ord, 51, 4);
     ArAddEli(&Ord, 61, 2);
     ArTulisIsi(Ord);
-    
+    printf("%d\n%c\n", sizeof(comm), comm[sizeof(comm)-1]);
     return 0;
 }
