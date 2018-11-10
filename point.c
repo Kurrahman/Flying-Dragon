8694660f964d51196252a0618c8e2f2035a02317
Doddy <doddyaditya78@gmail.com>
2018-11-10 09:48:11 +0700

    cok

--- point.c
+++ point.c
@@ -39,170 +39,25 @@ void TulisPOINT (POINT P)
 {
 	printf("(%.2f,%.2f)", Absis(P), Ordinat(P));
 }
-// Kelompok operasi relasional terhadap POINT //
-boolean EQ (POINT P1, POINT P2)
-/* Mengirimkan true jika P1 = P2 : absis dan ordinatnya sama */
-{
-	return((Absis(P1)== Absis(P2)) && (Ordinat(P1)==Ordinat(P2)));
-}
-boolean NEQ (POINT P1, POINT P2)
-/* Mengirimkan true jika P1 tidak sama dengan P2 */
-{
-	return((Absis(P1)!=Absis(P2)) || (Ordinat(P1)!=Ordinat(P2)));
-}
-// Kelompok menentukan di mana P berada //
-boolean IsOrigin (POINT P)
-/* Menghasilkan true jika P adalah titik origin */ 
-{
-	return((Absis(P)== 0) && (Ordinat(P)== 0));
-}
-boolean IsOnSbX (POINT P)
-/* Menghasilkan true jika P terletak Pada sumbu X */
-{
-	return(Ordinat(P)== 0);
-}
-boolean IsOnSbY (POINT P)
-/* Menghasilkan true jika P terletak pada sumbu Y */
-{
-	return(Absis(P)== 0);
-}
-int Kuadran (POINT P)
-/* Menghasilkan kuadran dari P: 1, 2, 3, atau 4 */
-/* Prekondisi : P bukan Titik Origin dan P tidak terletak di salah satu sumbu */
-{
-	/* KAMUS */
-	int quadran;
-	/* ALGORITMA */
-	if ((!IsOrigin(P)) && (!IsOnSbX(P)) && (!IsOnSbY(P)))
-	{
-		if ((Absis(P) > 0) && (Ordinat(P) > 0))
-		{
-			return(quadran = 1);
-		}
-		else
-		if ((Absis(P) < 0) && (Ordinat(P) > 0))
-		{
-			return(quadran = 2);
-		}
-		else
-		if ((Absis(P) < 0) && (Ordinat(P) < 0))
-		{
-			return(quadran = 3);
-		}
-		else
-		{
-			return(quadran = 4);
-		}
-	}
-	else
-	{
-		return quadran = 0;
-	}
-	
-}
 
 // KELOMPOK OPERASI LAIN TERHADAP TYPE //
-POINT NextX (POINT P)
+void GoRight (POINT *P)
 /* Mengirim salinan P dengan absis ditambah satu */
 {
-	return(MakePOINT((Absis(P) + 1), (Ordinat(P))));
+	*P = MakePOINT((Absis(*P) + 1), (Ordinat(*P)));
 }
-POINT NextY (POINT P)
+void GoLeft (POINT *P)
+/* Mengirim salinan P dengan absis dikurangi satu */
+{
+	*P = MakePOINT((Absis(*P) - 1), (Ordinat(*P)));
+}
+void GoUp (POINT *P)
 /* Mengirim salinan P dengan ordinat ditambah satu */
 {
-	MakePOINT((Absis(P)), (Ordinat(P) + 1));
+	*P = MakePOINT((Absis(*P)), (Ordinat(*P) + 1));
 }
-POINT PlusDelta (POINT P, float deltaX, float deltaY)
-/* Mengirim salinan P yang absisnya adalah Absis(P) + deltaX dan ordinatnya adalah Ordinat(P) + deltaY */
+void GoDown (POINT *P)
+/* Mengirim salinan P dengan ordinat dikurangi satu */
 {
-	MakePOINT((Absis(P) + deltaX), (Ordinat(P) + deltaY));
+	*P = MakePOINT((Absis(*P)), (Ordinat(*P) - 1));
 }
-POINT MirrorOf (POINT P, boolean SbX)
-/* Menghasilkan salinan P yang dicerminkan terhadap salah satu sumbu */
-/* Jika SbX bernilai true, maka dicerminkan terhadap sumbu X */
-/* Jika SbX bernilai false, maka dicerminkan terhadap sumbu Y */
-{
-	if (IsOnSbX(P))
-	{
-		return(MakePOINT((Absis(P)), (-(Ordinat(P)))));
-	}
-	else
-	if (!IsOnSbX(P))
-	{
-		return(MakePOINT((-(Absis(P))), (Ordinat(P))));
-	}
-}
-float Jarak0 (POINT P)
-/* Menghitung jarak P ke (0,0) */
-{
-	/* KAMUS */
-	float x;
-	/* ALGORITMA */
-	return(x = sqrt((Absis(P)*Absis(P)) + (Ordinat(P)*Ordinat(P))));
-}
-float Panjang (POINT P1, POINT P2)
-/* Menghitung panjang garis yang dibentuk P1 dan P2 */
-{
-	/* KAMUS */
-	float x;
-	/* ALGORITMA */
-	{
-	return(x = sqrt(((Absis(P2) - Absis(P1))*(Absis(P2) - Absis(P1))) + ((Ordinat(P2) - Ordinat(P1))*(Ordinat(P2) - Ordinat(P1)))));
-	}
-}
-/* Perhatikanlah bahwa di sini spec fungsi kurang baik sebab menyangkut ADT Garis. Tuliskan spec fungsi yang lebih tepat */
-void Geser (POINT *P, float deltaX, float deltaY)
-/* I.S. P terdefinisi */
-/* F.S. P digeser, absisnya sebesar deltaX dan ordinatnya sebesar deltaY */
-{
-	*P = MakePOINT((Absis(*P) + deltaX), (Ordinat(*P) + deltaY));
-}
-void GeserKeSbX (POINT *P)
-/* I.S. P terdefinisi */
-/* F.S. P berada pada sumbu X dengan absis sama dengan absis semula. */
-/* Proses : P digeser ke sumbu X. */
-/* Contoh : Jika koordinat semula (9,9), maka menjadi (9,0) */
-{
-	*P = MakePOINT((Absis(*P)), 0);
-}
-
-void GeserKeSbY (POINT *P)
-/* I.S. P terdefinisi */
-/* F.S. P berada pada sumbu Y dengan ordinat yang sama dengan semula. */
-/* Proses : P digeser ke sumbu Y. */
-/* Contoh : Jika koordinat semula (9,9), maka menjadi (0,9) */
-{
-	*P = MakePOINT(0, (Ordinat(*P)));
-}
-
-void Mirror (POINT *P, boolean SbX)
-/* I.S. P terdefinisi */
-/* F.S. P dicerminkan tergantung nilai SbX atau SbY */
-/* Jika SbX true maka dicerminkan terhadap sumbu X */
-/* Jika SbX false maka dicerminkan terhadap sumbu Y */
-{
-	if (IsOnSbX(*P))
-	{
-		(MakePOINT((Absis(*P)), (-(Ordinat(*P)))));
-	}
-	else
-	if (!IsOnSbX(*P))
-	{
-		(MakePOINT((-(Absis(*P))), (Ordinat(*P))));
-	}
-}
-
-void Putar (POINT *P, float Sudut)
-/* I.S. P terdefinisi */
-/* F.S. P digeser sebesar Sudut derajat dengan sumbu titik (0,0) */
-{
-	// KAMUS
-	float x, y;
-	// ALGORITMA
-    x = (cos(Sudut * (pi/180)) * Absis(*P)) - (Ordinat(*P) * sin(Sudut * (pi/180)));
-    y = (Absis(*P) * sin(Sudut * (pi/180))) + (Ordinat(*P) * cos(Sudut * (pi/180)));
-   	*P = MakePOINT(x, y);
-}
-
-
-	
